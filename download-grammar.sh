48c7d2b4605ef7122612587f61d470fa983c0e91
Sebastian Kürten <sebastian@topobyte.de>
2020-08-09 17:13:39 +0200

    Add script for downloading the latest version of the grammar

---
+++ download-grammar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+DIR=src/main/antlr/de/mobanisto/antlr/mysql
+mkdir -p "$DIR"
+wget -P "$DIR" https://github.com/antlr/grammars-v4/raw/master/sql/mysql/Positive-Technologies/MySqlLexer.g4
+wget -P "$DIR" https://github.com/antlr/grammars-v4/raw/master/sql/mysql/Positive-Technologies/MySqlParser.g4
